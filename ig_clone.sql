68116007fa840dbc2b290490b77c255bcf712443
Tao Chen <54517105+taochensyd@users.noreply.github.com>
2020-11-05 09:07:28 +1100

    Add files via upload

---
+++ ig_clone.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE ig_clones;
+USE ig_clone;
+
+CREATE TABLE USERS (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(255) UNIQUE,
+    created_at TIMESTAMP DEFAULT NOW()
+);
