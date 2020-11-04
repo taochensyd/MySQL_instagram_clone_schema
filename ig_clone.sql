69e6f79e08a77c920f79cb4b35d2557bf68a668b
Tao Chen <54517105+taochensyd@users.noreply.github.com>
2020-11-05 10:11:33 +1100

    reorder code order

--- ig_clone.sql
+++ ig_clone.sql
@@ -8,11 +8,6 @@ CREATE TABLE USERS (
     created_at TIMESTAMP DEFAULT NOW()
 );
 
-INSERT INTO users (username) VALUES
-('BlueTheCat'),
-('CharlieBrown'),
-('ColtSteele');
-
 CREATE TABLE photos (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
   image_url VARCHAR(255) NOT NULL,
@@ -20,11 +15,6 @@ CREATE TABLE photos (
   create_at TIMESTAMP DEFAULT NOW(),
   FOREIGN KEY(user_id) REFERENCES users(id)
 );
-
-INSERT INTO photos (image_url, user_id) VALUES
-('/alskjd76', 1),
-('/lkajsd98', 2),
-('/90jddlkj', 2);
 
 -- SELECT photos
 -- FROM photos
@@ -41,11 +31,6 @@ CREATE TABLE comments (
   FOREIGN KEY(photo_id) REFERENCES photos(id)
 );
 
-INSERT INTO comments(comment_text, user_id, photo_id) VALUES
-('Meow!', 1, 2),
-('Amazing Shot!', 3, 2),
-('I <3 This', 2, 1);
-
 CREATE TABLE likes (
   photo_id INTEGER NOT NULL,
   user_id INTEGER NOT NULL,
@@ -55,13 +40,6 @@ CREATE TABLE likes (
   PRIMARY KEY (user_id, photo_id)
 );
 
-INSERT INTO likes(user_id, photo_id) VALUES
-(1,1),
-(2,1),
-(1,2),
-(1,3),
-(3,3);
-
 CREATE TABLE follows (
   follower_id INTEGER NOT NULL,
   followee_id INTEGER NOT NULL,
@@ -70,12 +48,6 @@ CREATE TABLE follows (
   FOREIGN KEY(followee_id) REFERENCES users(id),
   PRIMARY KEY (follower_id, followee_id)
 )
-
-INSERT INTO follows(follower_id, followee_id) VALUES
-(1,2),
-(1,3),
-(3,1),
-(2,3);
 
 CREATE TABLE tags (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
@@ -90,3 +62,42 @@ CREATE TABLE photo_tags (
   FOREIGN KEY(tag_id) REFERENCES tags(id),
   PRIMARY KEY (photo_id, tag_id)
 )
+
+INSERT INTO users (username) VALUES
+('BlueTheCat'),
+('CharlieBrown'),
+('ColtSteele');
+
+INSERT INTO photos (image_url, user_id) VALUES
+('/alskjd76', 1),
+('/lkajsd98', 2),
+('/90jddlkj', 2);
+
+INSERT INTO comments(comment_text, user_id, photo_id) VALUES
+('Meow!', 1, 2),
+('Amazing Shot!', 3, 2),
+('I <3 This', 2, 1);
+
+INSERT INTO likes(user_id, photo_id) VALUES
+(1,1),
+(2,1),
+(1,2),
+(1,3),
+(3,3);
+
+INSERT INTO follows(follower_id, followee_id) VALUES
+(1,2),
+(1,3),
+(3,1),
+(2,3);
+
+INSERT INTO tags(tag_name) VALUES
+('asorable'),
+('cute'),
+('sunrise');
+
+INSERT INTO photo_tags (photo_id, tag_id)  VALUES
+(1,1),
+(1,2),
+(2,3),
+(3,2);
