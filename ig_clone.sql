bb3f3e0d54d344598f45009647703994eb23d66b
Tao Chen <54517105+taochensyd@users.noreply.github.com>
2020-11-05 09:21:57 +1100

    add data into comments table

--- ig_clone.sql
+++ ig_clone.sql
@@ -41,3 +41,8 @@ CREATE TABLE comments (
   FOREIGN KEY(photo_id) REFERENCES photos(id),
   FOREIGN KEY(user_id) REFERENCES users(id)
 );
+
+INSERT INTO comments(comment_text, user_id, photo_id) VALUES
+('Meow!', 1, 2),
+('Amazing Shot!', 3, 2),
+('I <3 This', 2, 1);
