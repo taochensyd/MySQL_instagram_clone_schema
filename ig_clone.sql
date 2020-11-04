635a0b2f44e7f565165e928f67666d8a0c9ab0e6
Tao Chen <54517105+taochensyd@users.noreply.github.com>
2020-11-05 09:23:50 +1100

    add likes table

--- ig_clone.sql
+++ ig_clone.sql
@@ -38,11 +38,19 @@ CREATE TABLE comments (
   user_id INTEGER NOT NULL,
   create_at TIMESTAMP DEFAULT NOW(),
   FOREIGN KEY(user_id) REFERENCES users(id),
-  FOREIGN KEY(photo_id) REFERENCES photos(id),
-  FOREIGN KEY(user_id) REFERENCES users(id)
+  FOREIGN KEY(photo_id) REFERENCES photos(id)
 );
 
 INSERT INTO comments(comment_text, user_id, photo_id) VALUES
 ('Meow!', 1, 2),
 ('Amazing Shot!', 3, 2),
 ('I <3 This', 2, 1);
+
+CREATE TABLE likes (
+  photo_id INTEGER NOT NULL,
+  user_id INTEGER NOT NULL,
+  create_at TIMESTAMP DEFAULT NOW(),
+  FOREIGN KEY(user_id) REFERENCES users(id),
+  FOREIGN KEY(photo_id) REFERENCES photos(id),
+  PRIMARY KEY (user_id, photo_id)
+);
